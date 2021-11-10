62d291b83ce7a38405def06ffdb867355a8b45d5
Yashar Sarabi <77018659+yasharjs@users.noreply.github.com>
2021-11-10 01:22:30 -0500

    able to save tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,7 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 var pageContentEl = document.querySelector("#page-content");
 var tasksInProgressEl = document.querySelector("#tasks-in-progress");
 var tasksCompletedEl = document.querySelector("#tasks-completed");
+var tasks = [];
 
 
 var taskFormHandler = function(){
@@ -30,7 +31,8 @@ var taskFormHandler = function(){
     else{
         var taskDataObj = {
             name: taskNameInput,
-            type: taskTypeInput
+            type: taskTypeInput,
+            status: "to do"
         };
         createTaskEl(taskDataObj);
     }
@@ -48,7 +50,16 @@ var completeEditTask = function(taskName, taskType, taskId){
     taskSelected.querySelector("h3.task-name").textContent = taskName;
     taskSelected.querySelector("span.task-type").textContent = taskType;
 
-    alert("Task updated!");
+    // loop through tasks array and task object with new content
+    for (var i = 0; i < tasks.length; i++){
+        if(tasks[i].id === parseInt(taskId)){
+            tasks[i].name = taskName;
+            task[i].type = taskType;
+        }
+    }
+
+    saveTasks();
+
 
     formEl.removeAttribute("data-task-id");
     document.querySelector("#save-task").textContent = "Add Task";
@@ -77,8 +88,15 @@ var createTaskEl = function(taskDataObj){
       // add entire list item to list
       tasksToDoEl.appendChild(listItemEl);
 
+      taskDataObj.id = taskIdCounter;
+      tasks.push(taskDataObj);
+
       //increase task counter for next unique id
       taskIdCounter++;
+
+      saveTasks();
+
+   
 }
 
 var createTaskActions = function(taskId){
@@ -125,6 +143,21 @@ var createTaskActions = function(taskId){
 var deleteTask = function(taskId){
     var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
     taskSelected.remove();
+
+    // create new array to hold updated list of tasks
+    var updatedTaskArr = [];
+
+    // loop through current tasks
+    for (var i = 0; i < tasks.length; i++){
+          // if tasks[i].id doesn't match the value of taskId, let's keep that task and push it into the new array
+        if(tasks[i].id !== parseInt(taskId)){
+            updatedTaskArr.push(tasks[i]);
+        }
+    }
+    // reassign tasks array to be the same as updatedTaskArr
+    tasks = updatedTaskArr;
+
+    saveTasks();
 }
 
 var editTask = function(taskId){
@@ -162,14 +195,15 @@ var taskButtonHandler = function(event){
 }
 
 var taskStatusChangeHandler = function(event){
-      // get the task item's id
-       var taskId = event.target.getAttribute("data-task-id");
+    // get the task item's id
+    var taskId = event.target.getAttribute("data-task-id");
 
-      // get the currently selected option's value and convert to lowercase
-        var statusValue = event.target.value.toLowerCase();
+    // get the currently selected option's value and convert to lowercase
+    var statusValue = event.target.value.toLowerCase();
 
-      // find the parent task item element based on the id
-        var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+    // find the parent task item element based on the id
+    var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+    
     if(statusValue === "to do"){
         tasksToDoEl.appendChild(taskSelected);
     }
@@ -179,6 +213,19 @@ var taskStatusChangeHandler = function(event){
     else if(statusValue === "completed"){
         tasksCompletedEl.appendChild(taskSelected);
     }
+
+    // update task's in tasks array
+    for(var i = 0; i < tasks.length; i++){
+        if(tasks[i].id === parseInt(taskId)){
+            tasks[i].status = statusValue;
+        }
+    }
+
+    saveTasks();
+}
+
+var saveTasks = function(){
+    localStorage.setItem("tasks",JSON.stringify(tasks));
 }
 
 formEl.addEventListener("submit", taskFormHandler);
